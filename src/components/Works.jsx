0c0c1a88c6706b281e7ac8f782a0fad4cf00922b
Rounit-1st <154735289+Rounit-1st@users.noreply.github.com>
2025-09-06 11:47:35 +0530

    Update Works.jsx fixed missing images and videos

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -40,16 +40,16 @@ const StyledImages=(props)=>{
       <div className='bg-[url("./Projects/projectOneImg.png")] bg-center bg-cover bg-no-repeat w-64 h-48 sm:w-96 md:h-64 rounded-lg md:hover:w-[450px] md:hover:h-[300px] hover:z-20 hover:cursor-pointer border-2 border-black  rotate-12 scale-y-40 absolute hover:rotate-0 '></div>
       <div className='bg-no-repeat w-64  sm:w-96  rounded-lg md:hover:w-[450px] hover:z-20 hover:cursor-pointer border-2 border-black -rotate-12 absolute top-64  hover:rotate-0'>
           <video autoPlay muted loop id="myVideo">
-            <source src="./Projects/projectOneVid.mp4" type="video/mp4"/>
+            <source src="/Projects/projectOneVid.mp4" type="video/mp4"/>
           </video>
       </div>
       <div className='bg-no-repeat w-64  sm:w-96  rounded-lg md:hover:w-[450px] hover:z-20 hover:cursor-pointer border-2 border-black rotate-12 absolute top-32 right-0 md:left-64  hover:rotate-0'>
         <video autoPlay muted loop id="myVideo">
-            <source src="./Projects/projectOneVid2.mp4" type="video/mp4"/>
+            <source src="/Projects/projectOneVid2.mp4" type="video/mp4"/>
         </video>
       </div>
       <a href={props.link}>
-        <div className='bg-[url("./Projects/projectLogo.png")] bg-center bg-cover bg-no-repeat w-32 h-32 rounded-lg  border-4 border-black absolute top-32 right-1/3 md:left-64 '></div>
+        <div className='bg-[url("/Projects/projectLogo.png")] bg-center bg-cover bg-no-repeat w-32 h-32 rounded-lg  border-4 border-black absolute top-32 right-1/3 md:left-64 '></div>
       </a>
     </div>
   )
