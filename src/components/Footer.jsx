d5873fa5ba8e3756adb9f90b71fa33fba9279b42
Rounit-1st <154735289+Rounit-1st@users.noreply.github.com>
2025-09-08 10:50:16 +0530

    ugly border of video fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer=()=>{
     return (
         <div className="bg-slate-700 text-center p-4">
             <div className="socialLinks flex flex-rows justify-around p-4 mx-16 md:mx-96">
-               <a href="https://github.com/Rounit-1st"><img src="/Hyper_Image/github.png " className="h-16"  alt="" /></a>
-               <a href="https://codeforces.com/profile/forcerounit"><img src="/img/icons8-codeforces-96.png" className="h-16"  alt="" /></a>
-               <a href="https://mail.google.com/mail/?view=cm&fs=1&to=rounitpeppa@gmail.com"><img src="/img/icons8-gmail.svg " className="h-16"  alt="" /></a>
-               <a href="https://www.codechef.com/users/chefrounit"><img src="/img/icons8-codechef.svg" className="h-16"  alt="" /></a>
+               <a href="https://github.com/Rounit-1st"><img src="/Hyper_Image/github.png " className="h-10 md:h-16"  alt="" /></a>
+               <a href="https://codeforces.com/profile/forcerounit"><img src="/img/icons8-codeforces-96.png" className="h-10 md:h-16"  alt="" /></a>
+               <a href="https://mail.google.com/mail/?view=cm&fs=1&to=rounitpeppa@gmail.com"><img src="/img/icons8-gmail.svg " className="h-10 md:h-16"  alt="" /></a>
+               <a href="https://www.codechef.com/users/chefrounit"><img src="/img/icons8-codechef.svg" className="h-10 md:h-16"  alt="" /></a>
             </div>
 
             © Copylefted by Mr Dogesh company
